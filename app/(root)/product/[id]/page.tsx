dc977e1135cad026176473c21ecea3a43fe4d845
AFilippov <64398851+WebAFilippov@users.noreply.github.com>
2024-08-08 03:05:41 +0700

    update

--- app/(root)/product/[id]/page.tsx
+++ app/(root)/product/[id]/page.tsx
@@ -1,7 +1,48 @@
+import { Container, GroupVariants, ProductImage, Title } from '@/components/shared'
+import { prisma } from '@/prisma/prisma-client'
+import { notFound } from 'next/navigation'
 import React from 'react'
 
-const ProductPage = ({ params: { id } }: { params: { id: string } }) => {
-  return <div>{id}</div>
+const ProductPage = async ({ params: { id } }: { params: { id: string } }) => {
+  const product = await prisma.product.findFirst({ where: { id: Number(id) } })
+
+  if (!product) {
+    return notFound()
+  }
+
+  return (
+    <Container className='flex flex-col my-10 min-h-[500px]'>
+      <div className='flex flex-1'>
+        <ProductImage imageUrl={product.imageUrl} size={20} alt={product.name} />
+
+        <div className='w-[490px] bg-[#fcfcfc] p-7'>
+          <Title text={product.name} size='md' className='font-extrabold mb-1' />
+          <p className='text-gray-400'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, rem?
+          </p>
+
+          <GroupVariants
+            selectedVarintId={3}
+            items={[
+              {
+                name: 'Маленькая',
+                value: 1,
+              },
+              {
+                name: 'Средняя',
+                value: 2,
+              },
+              {
+                name: 'Большая',
+                value: 3,
+                disabled: true,
+              },
+            ]}
+          />
+        </div>
+      </div>
+    </Container>
+  )
 }
 
 export default ProductPage
---
+++ components/shared/group-variants.tsx
@@ -0,0 +1,43 @@
+'use client'
+import { cn } from '@/lib/utils'
+import React from 'react'
+
+interface Variant {
+  name: string
+  value: number
+  disabled?: boolean
+}
+
+interface Props {
+  items: Variant[]
+  selectedVarintId: Variant['value']
+  onClick?: (value: Variant['value']) => void
+  className?: string
+}
+
+export const GroupVariants: React.FC<Props> = ({ items, selectedVarintId, onClick, className }) => {
+  return (
+    <div
+      className={cn(
+        'flex flex-1 items-center justify-between bg-[#f1f0f5] p-1 select-none rounded-3xl overflow-auto',
+        className,
+      )}
+    >
+      {items.map((item) => (
+        <button
+          key={item.value}
+          className={cn(
+            'flex flex-1 justify-center items-center px-5 h-[30px] text-sm rounded-3xl transition-all duration-300 cursor-pointer',
+            {
+              'bg-white shadow-custom': selectedVarintId === item.value,
+              'text-gray-500 opacity-50 cursor-not-allowed': item.disabled,
+            },
+          )}
+          onClick={() => onClick?.(item.value)}
+        >
+          {item.name}
+        </button>
+      ))}
+    </div>
+  )
+}
